5262b0b836904f105797b2f08b1fa075da99a77e
shahadqari <qari.s@husky.neu.edu>
2017-04-19 21:02:43 -0400

    week-13

---
+++ week-13/task-1/script.js
@@ -0,0 +1,27 @@
+var array; // declartion/ definition
+array = ["a", "b",undefined, "c"]; //initialization
+
+//console.log(array);
+var randomName =10;
+//console.log(randomName);
+//console.log(array[2])
+//console.log(array[4])
+
+
+var obj= {name: "Shahad",
+          University: "NEU",
+          forEach: function (){console.log("log obj");}
+};
+//console.log(obj.forEach);
+obj.forEach();
+
+varx=array [4];
+//console.log(x.length);
+
+
+console.log("this is a string",9,[9,10,11,],obj,obj);
+var object={odd: [1,3,5,7,9]};
+object.odd.push(11);
+object.odd.length
+object.odd.push(13)
+object;odd
